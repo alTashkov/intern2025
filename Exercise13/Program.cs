32f1169a8d36afaf236bc527875d288bba56b44d
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-07-17 14:01:14 +0300

    Small change

--- Exercise13/Program.cs
+++ Exercise13/Program.cs
@@ -5,12 +5,14 @@
     {
         Console.Write("First floating-point number: ");
         double a = double.Parse(Console.ReadLine());
+
         Console.Write("Second floating-point number: ");
         double b = double.Parse(Console.ReadLine());
 
         double eps = 0.000001;
 
         bool areEqual = Math.Abs(a - b) < eps;
-        Console.WriteLine(areEqual);
+        Console.WriteLine(areEqual); 
+        
     }
 }
