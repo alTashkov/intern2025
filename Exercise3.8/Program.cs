7e0285ed0e42cf8d69dc616d84190046553e68cf
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-07-23 11:54:42 +0300

    Add ex. 3.8

---
+++ Exercise3.8/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+
+        int number = int.Parse(Console.ReadLine());
+        while (number > 100 || number < 1)
+        {
+            Console.WriteLine("Number must be in the range [1;100]!");
+            Console.Write("Please re-enter the number: ");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        bool isPrime;
+
+        int count = 0;
+
+        if (number > 0)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                    //Console.WriteLine($"{number} % {i} = {number % i}");
+                }
+            }
+
+            isPrime = count == 2 ? true : false;
+        }
+        else
+        {
+            isPrime = false;
+        }
+        
+        Console.WriteLine(isPrime);
+
+    }
+}
